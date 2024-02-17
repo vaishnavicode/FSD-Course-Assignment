f2151d49ab2bf788818a710a16091975431a7a2c
Vaishnavi Singh <vaishnavicode3@gmail.com>
2024-02-18 00:11:27 +0530

    Updated solution of Module 4

--- Module4/Script.js
+++ Module4/Script.js
@@ -30,20 +30,20 @@ WARNING!!! WARNING!!!
 
 (function () {
 
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    
-    for (var i = 0; i < names.length; i++) {
-      var firstLetter = names[i].charAt(0).toLowerCase();
-    
-      if (firstLetter === 'j') {
-        byeSpeaker(names[i]);
-      }
-      else {
-        helloSpeaker(names[i]);
-      }
-    }
-    
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
 })();
-    
-    
-    
+
+
+
